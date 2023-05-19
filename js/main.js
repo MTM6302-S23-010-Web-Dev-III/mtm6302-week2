b65bf14b5441399fbe6404f5c28e36d4fc0429dc
Abhay Sharma <abhaysharmawebd@gmail.com>
2023-05-19 11:19:54 -0400

    completes week 2 demo

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// console.log('JS file included!')
+/* 
+  The above line of code is commented out 
+  using a single line comment, the shortcut is control + / or command + /
+
+  This comment block is a multi-line comment, the shortcut is shift+alt+A
+*/
+let myName = "Abhay"
+console.log(myName)
+
+myName = "Abhay Sharma"
+console.log(myName)
+
+// declaring a variable, the default value will be set to undefined
+let myProfession
+
+// assigning a value to the variable
+myProfession = "Professor"
+console.log(myProfession)
+
+const myAddress = "Earth"
+console.log(myAddress)
+
+const numA = 4
+const numB = 7
+
+console.log(numA + numB)
+console.log(numA * numB)
+
+console.log(numA === numB)
+console.log(numA < numB)
+
+console.log(numA + 3 == numB && numA != numB)
+
+const numC = 4.9871
+
+console.log(numC.toFixed(2)) // toFixed is a method of a number
+
+console.log(parseInt(numC)) // parseInt is s function
+
+const myFirstName = "Abhay"
+const dogs = 2
+const dog1Name = "Simba"
+const dog2Name = "Bufi"
+
+console.log("I am " + myFirstName + ".\n I have " + dogs + " dogs, " + dog1Name + " & " + dog2Name)
+
+console.log(`I am ${myFirstName}. I have ${dogs} dogs, ${dog1Name} & ${dog2Name}`)
+
